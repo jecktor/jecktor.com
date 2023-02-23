807ca920755e7772de6e98e809ac55a7f0d84d98
jecktor <jecktor@proton.me>
2023-02-22 21:27:23 -0600

    Removed theme switcher

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
